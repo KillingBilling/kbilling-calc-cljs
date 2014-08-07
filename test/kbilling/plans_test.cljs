b0a8d206302fe5dd7e53841af0ba8558c503ef30
ivan <i.mikushin@gmail.com>
2014-08-08 03:33:45 +0600

    rm old commented-out code

--- test/kbilling/plans_test.cljs
+++ test/kbilling/plans_test.cljs
@@ -13,13 +13,5 @@
                 (get-in (p/load-plan "test/kbilling/plans/examples/basic") ["cycles" "$subscription" "$begin"]))))
 
 
-;should PASS
-;(deftest somewhat-less-wat
-;         (is (= "{}[]" (+ {} []))))
-
-;should PASS
-;(deftest javascript-allows-div0
-;         (is (= js/Infinity (/ 1 0) (/ (int 1) (int 0)))))
-
 (node/enable-util-print!)
 (set! *main-cli-fn* t/run-all-tests)
