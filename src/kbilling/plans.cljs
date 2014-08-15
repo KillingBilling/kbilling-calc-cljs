91714989eec6889b781ee5cff84a691efce9fc20
ivan <i.mikushin@gmail.com>
2014-08-15 19:06:49 +0600

    load-cost-fn

--- src/kbilling/plans.cljs
+++ src/kbilling/plans.cljs
@@ -1,11 +1,17 @@
-(ns kbilling.plans
-  (:require [kbilling.plans.transform :as tf]))
+(ns kbilling.plans)
 
 (def reflect-fn (js/require "function-to-string"))
 
+(defn global-var-name [path var-name]
+  (let [parts (clojure.string/split var-name #"\$(?!cost)")
+        cur-cycle (case (path 0) :$cycles (name (path 1)) nil)]
+    (if (and cur-cycle (= 2 (count parts)))
+      (str cur-cycle "$" var-name)
+      var-name)))
+
 (defn load-cost-fn [f path]
-  (let [params (vec (map keyword (.-params (reflect-fn f))))] ;TODO prefix local params with cycle key
-    (fn [vars] (apply f (map #(vars %) params)))))          ;TODO test
+  (let [params (vec (map (comp keyword #(global-var-name path %)) (.-params (reflect-fn f))))]
+    (fn [vars] (apply f (map #(vars %) params)))))
 
 (defn load-v [x path]
   (cond
--- test/kbilling/plans_test.cljs
+++ test/kbilling/plans_test.cljs
@@ -1,6 +1,6 @@
 (ns kbilling.plans-test
   (:require-macros [cemerick.cljs.test
-                    :refer (is deftest with-test run-tests testing test-var)])
+                    :refer [is deftest with-test run-tests testing test-var]])
   (:require [kbilling.plans :as p]
             [cemerick.cljs.test :as t]))
 
@@ -9,14 +9,22 @@
 
 (def basic-plan (p/load-plan "test/kbilling/plans/examples/basic"))
 
+(deftest global-var-name-test
+  (is (= "monthly$coverage$sum"
+         (p/global-var-name [:$cycles :monthly :rub] "coverage$sum")))
+  (is (= "monthly$coverage$sum"
+         (p/global-var-name [:$cycles :monthly :rub] "monthly$coverage$sum")))
+  (is (= "daily$coverage$sum"
+         (p/global-var-name [:$cycles :monthly :rub] "daily$coverage$sum"))))
+
 (deftest load-cost-fn-test
   (let [f (js/eval "(function(coverage$sum) { return coverage$sum * 0.3 })")
-        cost-fn (p/load-cost-fn f [])]
-    (is (= 60 (cost-fn {:coverage$sum 200})))))
+        cost-fn (p/load-cost-fn f [:$cycles :monthly :rub])]
+    (is (= 60 (cost-fn {:monthly$coverage$sum 200})))))
 
-#_(deftest cost-function
-  (let [cost (get-in basic-plan [:cycles :monthly :rub :$cost])]
-    (is (= "60" (str (cost {"coverage$sum" 200}))))))
+(deftest cost-function
+  (let [cost (get-in basic-plan [:$cycles :monthly :rub :$cost])]
+    (is (= "60" (str (cost {:monthly$coverage$sum 200}))))))
 
 (deftest load-plan
   (is (= [:$cycles :$values :$notifications]
