1bd83c0860c39f2e1655ab68b465ff41132784f6
ivan <i.mikushin@gmail.com>
2014-08-18 10:11:32 +0600

    load value functions

--- src/kbilling/plans.cljs
+++ src/kbilling/plans.cljs
@@ -4,12 +4,14 @@
 
 (defn global-var-name [path var-name]
   (let [parts (clojure.string/split var-name #"_")
-        cur-cycle (case (path 0) :$cycles (name (path 1)) nil)]
+        cur-cycle (case (path 0)
+                    :$cycles (name (path 1))
+                    nil)]
     (if (and cur-cycle (= 2 (count parts)))
       (str cur-cycle "_" var-name)
       var-name)))
 
-(defn load-cost-fn [f path]
+(defn load-fn [f path]
   (let [params (vec (map (comp keyword #(global-var-name path %)) (.-params (reflect-fn f))))]
     (fn [vars] (apply f (map #(vars %) params)))))
 
@@ -17,10 +19,11 @@
   (cond
     (map? x)
     (into {} (for [[k v] x]
-               [k (case k
-                    :$begin (set (map keyword v))
-                    :$cost (load-cost-fn v path)
-                    (load-v v (conj path k)))]))
+               [k (cond
+                    (= k :$begin) (set (map keyword v))
+                    (= k :$cost) (load-fn v path)
+                    (= path [:$values]) (load-fn v path)
+                    :else (load-v v (conj path k)))]))
 
     :else x))
 
--- test/kbilling/plans_test.cljs
+++ test/kbilling/plans_test.cljs
@@ -17,14 +17,18 @@
   (is (= "daily_coverage_sum"
          (p/global-var-name [:$cycles :monthly :rub] "daily_coverage_sum"))))
 
-(deftest load-cost-fn-test
+(deftest load-fn-test
   (let [f (js/eval "(function(coverage_sum) { return coverage_sum * 0.3 })")
-        cost-fn (p/load-cost-fn f [:$cycles :monthly :rub])]
+        cost-fn (p/load-fn f [:$cycles :monthly :rub])]
     (is (= 60 (cost-fn {:monthly_coverage_sum 200})))))
 
 (deftest cost-function
-  (let [cost (get-in basic-plan [:$cycles :monthly :rub :$cost])]
-    (is (= "60" (str (cost {:monthly_coverage_sum 200}))))))
+  (let [cost-fn (get-in basic-plan [:$cycles :monthly :rub :$cost])]
+    (is (= "60" (str (cost-fn {:monthly_coverage_sum 200}))))))
+
+(deftest value-function
+  (let [value-fn (get-in basic-plan [:$values :rubOrCost])]
+    (is (= "3000" (str (value-fn {:monthly_rub_$cost 3000, :rub 2000}))))))
 
 (deftest load-plan
   (is (= [:$cycles :$values :$notifications]
