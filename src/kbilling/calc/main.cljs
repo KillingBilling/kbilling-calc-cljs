1f0943825aedc28cfba2480f87eb6caa2276e8ff
ivan <i.mikushin@gmail.com>
2014-08-27 12:03:19 +0600

    pipe-through

--- src/kbilling/calc/main.cljs
+++ src/kbilling/calc/main.cljs
@@ -8,11 +8,11 @@
 
 (defn transform [s] (string/upper-case s))
 
+(defn pipe-through [f in out]
+  (-> in (.pipe (split (fn [s] (str (f s) "\n")))) (.pipe out)))
+
 (defn -main [& args]
-  (-> js/process
-      .-stdin
-      (.pipe (split (fn [s] (str (transform s) "\n"))))
-      (.pipe (.-stdout js/process))))
+  (pipe-through transform (.-stdin js/process) (.-stdout js/process)))
 
 ;(node/enable-util-print!)
 (set! *main-cli-fn* -main)
